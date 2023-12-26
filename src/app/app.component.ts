a9d89e577615a43d14a4ae91ab82c9326a564c78
polmariin01 <pol.marin.gargallo@gmail.com>
2023-12-26 12:49:29 +0100

    es mostra l'alfabet i es pot clicar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { LlistaAlfabetComponent } from './llista-alfabet/llista-alfabet.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, RouterModule, LlistaAlfabetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

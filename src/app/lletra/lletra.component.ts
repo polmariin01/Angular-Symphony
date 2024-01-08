875a02db9109e68265f5e38fca5cf83aced2f65e
polmariin01 <pol.marin.gargallo@gmail.com>
2024-01-08 13:08:53 +0100

    trending status updates correctly

--- src/app/lletra/lletra.component.ts
+++ src/app/lletra/lletra.component.ts
@@ -4,17 +4,21 @@ import { LlistaApiService } from '../llista-api.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Observable, Observer } from 'rxjs';
+import { COMPOSITION_BUFFER_MODE } from '@angular/forms';
 
 interface Composer {
-  id: string;
+  id: string;             // Propietats originals
   name: string;
   complete_name: string;
   birth: string;
-  death: string | null;
+  death: string;
   epoch: string;
+  portrait: string;
 
-  isTrending?: boolean; // Add the optional property here
-  works?: any;
+  expanded?: boolean;
+  count?: number;         // Nombre de clics
+  isTrending?: boolean;   // Si es tendencia o no
+  works?: any;            // Nombre d'obres
 }
 
 
@@ -28,7 +32,8 @@ interface Composer {
 })
 export class LletraComponent {
   letter: string | null = null;
-  data: any;
+  data: Composer[] = [];
+  //currentTrending: any;
 
   constructor(private route : ActivatedRoute, private apiService: LlistaApiService) {}
 
@@ -42,11 +47,11 @@ export class LletraComponent {
 
 
   //Generate interaction with the api since an author has been clicked on
-  onClick(composer: any): void {
+  onClick(composer: Composer): void {
     if (composer.expanded) {
       composer.expanded = false;
     } else {
-      //Request a la API que porta el recompte de tendencia
+      // Request a la API que porta el recompte de tendencia
       // aqui vaig fer la crida a la api d'aquesta forma, després he vist que es millor creant un observer
       this.apiService.generateInteraction(composer.id).subscribe(
         (response) => {
@@ -60,7 +65,7 @@ export class LletraComponent {
       //console.log('this id: ', composer.id);
       composer.expanded = true;
 
-      this.createData();
+      this.updateTrending();
     }
   }
 
@@ -82,11 +87,16 @@ export class LletraComponent {
           item.death = 'present';
         }          
       });
-      item.isTrending = await this.getTrendingStatus(item); // Afegeix propietat booleana de tendencia per saber si s'ha d'afegir algo visual per fer notar que es tendencia
+      let trendStatus : any = await this.getTrendingStatus(item);
+      item.isTrending = trendStatus.isTrending;
+      item.count = trendStatus.countedInteractions;
+//      item.isTrending = await this.getTrendingStatus(item); // Afegeix propietat booleana de tendencia per saber si s'ha d'afegir algo visual per fer notar que es tendencia
       item.works = await this.getWorks(item);               // Afegeix una llista a cada compositor amb noms d'obres seves.
     };
 
-    console.log("Data object.");
+    this.updateTrending();
+
+    console.log("Objecte amb totes les dades");
     console.log(this.data);
   }
 
@@ -97,7 +107,7 @@ export class LletraComponent {
       const observer: Observer<any> = {
         next: (response) => {
           if ('composers' in response) {
-            this.data = response.composers as Composer[];
+            this.data = response.composers as any;
             //console.log(this.data);
           }
           resolve();
@@ -117,24 +127,7 @@ export class LletraComponent {
   }
   
 
-
-  //deprecated  - no s'utilitza pero la deixo per si un cas
-  getComposerData(composer: any): void {
-    //Request a la API que porta el recompte de 
-    this.apiService.getComposerData(composer.id).subscribe(
-      (response) => {
-        //console.log(response);
-        composer.isTrending = response.isTrending;
-        //console.log(composer.isTrending);
-      },
-      (error) => {
-        console.error('Error getting composer data', error);
-      }
-    );
-    //console.log('this id: ', row.id);
-  }
-
-
+  // En aquest cas la crida és a la API
   async getTrendingStatus(composer: Composer): Promise<boolean> {
     // Request to the API for the count of trending status
     //console.log('getTrendingStatus() - composer:', composer.name, composer.id);
@@ -143,9 +136,9 @@ export class LletraComponent {
     return new Promise<boolean>((resolve, reject) => {
       const observer: Observer<any> = {
         next: (response) => {
-          //console.log(response);
+          console.log(response);
           //console.log('getTrendingStatus() - ' + response.isTrending);
-          resolve(response.isTrending);
+          resolve(response);
         },
         error: (error) => {
           console.error('Error getting composer data', error);
@@ -163,7 +156,7 @@ export class LletraComponent {
   
 
   //TODO
-  async getWorks(composer: any): Promise<void> {
+  async getWorks(composer: Composer): Promise<void> {
     // GET /work/list/composer/129/genre/all.json
     return new Promise<void>((resolve, reject) => {
       
@@ -192,6 +185,84 @@ export class LletraComponent {
       // Call the API using the observer
       this.apiService.getWorks(composer.id).subscribe(observer);
     });
+  }
+
+
+  /**function findPositionByID(composers, ID) {
+    const index = composers.findIndex(composer => composer.id === targetId);
+  }*/
+
+  // Actualitza l'element
+  updateTrending(): void {
+//    updateTrending(updatedComposer: any): void {
+  // Anava a fer una funcio que mires només l'antic i el més nou, fent-lo bastant més eficient pero menys robust.
+  // Al no haver moltes dades no crec que calgui fer-ho tant eficient i així es pot reutilitzar la formula per utilitzar-la al init també.
+    console.log("UPDATE TRENDING");
+
+    let maxCount = 0;
+    let trendingComposer: Composer | null = null;
+
+    // Busca quin es el que te el count més alt
+    this.data.forEach((composer: Composer) => {
+      if (composer.count != null && composer.count >= maxCount) {
+        maxCount = composer.count;
+        trendingComposer = composer;
+      }        
+    });
+
+/*
+    for (const composer of this.data) {
+      if (composer.count != null && composer.count >= maxCount) {
+        maxCount = composer.count;
+        trendingComposer = composer;
+      }
+    }
+*/
+
+    this.data.forEach((composer: Composer) => {
+      if (composer === trendingComposer) {
+        composer.isTrending = true;
+      } else {
+        composer.isTrending = false;
+      }     
+    });
+
+    /*
+    if (trendingComposer != null && trendingComposer.name) {
+      console.log("Trendng for letter " + this.letter, ":", trendingComposer.name);
+    }
+    */
+    // Actualitza la propietat isTrending de tots els compositors
+    /*
+    for (const composer of this.data) {
+      if (composer === trendingComposer) {
+        composer.isTrending = true;
+      } else {
+        composer.isTrending = false;
+      }
+    }
+    */
+  }
+
+
+
+
+
+
+  //deprecated  - no s'utilitza pero la deixo per si un cas
+  getComposerData(composer: any): void {
+    //Request a la API que porta el recompte de 
+    this.apiService.getComposerData(composer.id).subscribe(
+      (response) => {
+        //console.log(response);
+        composer.isTrending = response.isTrending;
+        //console.log(composer.isTrending);
+      },
+      (error) => {
+        console.error('Error getting composer data', error);
+      }
+    );
+    //console.log('this id: ', row.id);
   }
 
 
